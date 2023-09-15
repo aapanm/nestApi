f0dea10a9deee64577bdc5b1ac545807b05de2a3
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-09-15 22:24:27 +0600

    config added

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,15 +1,18 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
 import { Task } from 'src/tasks/task.entity';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModule = {
-  type: 'postgres',
-  host: '127.0.0.1',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
+  type: dbConfig.type,
+  host: process.env.HOST || dbConfig.host,
+  port: dbConfig.port,
+  username: process.env.DB_USERNAME || dbConfig.username,
+  password: process.env.DB_PASSWORD || dbConfig.password,
   schema: 'public',
-  database: 'task-management',
+  database: dbConfig.database,
   entities: [Task, User],
-  synchronize: true,
+  synchronize: process.env.SYNC || dbConfig.synchronize,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('bootstrap');
+
+  const serverConfig = config.get('server');
+  const port = serverConfig.port;
+  await app.listen(port);
+
+  logger.log(`server is up at port ${port}`);
 }
 bootstrap();
